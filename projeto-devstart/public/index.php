69af3f502ff5c3f5546078ad8a4ea898731a4e2c
nely de faria <nelly.nf54@gmail.com>
2022-06-09 16:21:17 -0300

    Criando rotas

--- projeto-devstart/public/index.php
+++ projeto-devstart/public/index.php
@@ -2,14 +2,29 @@
 
 include '../vendor/autoload.php';
 
+use App\Controller\CategoryController;
+use App\Controller\ErrorController;
 use App\Controller\IndexController;
 use App\Controller\ProductController;
 
-$c = new IndexController();
-// $c->indexAction();
-$c->loginAction();
+$url = explode('?', $_SERVER['REQUEST_URI'])[0];
+echo $url . '<br>';
 
-$p = new ProductController();
-$p->listAction();
-$p->addAction();
-$p->editAction();
+$routes = [
+    '/projeto-devstart/public/' => [
+        'controller' => IndexController::class,
+        'method' => 'indexAction',
+    ],
+    '/projeto-devstart/public/produtos' => [
+        'controller' => ProductController::class,
+        'method' => 'listAction',
+    ],
+];
+
+if (false === isset($routes[$url])) {
+    $e = new ErrorController();
+    $e->notFoundAction();
+}
+
+var_dump($routes[$url]);
+
