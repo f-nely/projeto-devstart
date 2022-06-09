cb82a8a0c2924ab83f1d77827ae93fb72e103151
nely de faria <nelly.nf54@gmail.com>
2022-06-09 15:34:48 -0300

    Criando arquivo index.php

---
+++ projeto-devstart/public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+include '../vendor/autoload.php';
+
+use App\Controller\IndexController;
+use App\Controller\ProductController;
+
+$c = new IndexController();
+// $c->indexAction();
+$c->loginAction();
+
+$p = new ProductController();
+$p->listAction();
+$p->addAction();
+$p->editAction();
