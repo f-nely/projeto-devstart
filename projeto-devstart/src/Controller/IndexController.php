08e8744a16ae1ba93c45e4009d84484c8e265113
nely de faria <nelly.nf54@gmail.com>
2022-06-09 15:43:25 -0300

    Herdando da classe AbstractController

--- projeto-devstart/src/Controller/IndexController.php
+++ projeto-devstart/src/Controller/IndexController.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-class indexController
+class indexController extends AbstractController
 {
     public function indexAction(): void
     {
-        // echo dirname(__DIR__);
-        // echo __DIR__;
-        include dirname(__DIR__) . '/View/index/index.php';
+        // include dirname(__DIR__) . '/View/index/index.php';
+        parent::render('index/index');
     }
 
     public function loginAction(): void
     {
-        include dirname(__DIR__) . '/View/index/login.php';
+        // include dirname(__DIR__) . '/View/index/login.php';
+        parent::render('index/login');
     }
 }
