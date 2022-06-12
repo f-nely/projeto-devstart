2e01381bc43fb44cae3c9f733842206727dc39be
nely de faria <nelly.nf54@gmail.com>
2022-06-12 17:59:07 -0300

    Listando categorias

--- projeto-devstart/src/Controller/CategoryController.php
+++ projeto-devstart/src/Controller/CategoryController.php
@@ -4,20 +4,28 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Connection\Connection;
+
 class CategoryController extends AbstractController
 {
     public function listAction(): void
     {
-        parent::render('category/list');
+        $con = Connection::getConnection();
+
+        $result = $con->prepare('SELECT * FROM tb_category');
+
+        $result->execute();
+
+        parent::render('category/list', $result);
     }
 
     public function addAction(): void
     {
-        parent::render('category/add');
+        // parent::render('category/add');
     }
 
     public function editAction(): void
     {
-        parent::render('category/edit');
+        // parent::render('category/edit');
     }
 }
