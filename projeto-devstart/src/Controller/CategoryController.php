129ce845e2e6345e21723f8858530f38d0683172
nely de faria <nelly.nf54@gmail.com>
2022-06-13 15:09:31 -0300

    Implementando método addAction

--- projeto-devstart/src/Controller/CategoryController.php
+++ projeto-devstart/src/Controller/CategoryController.php
@@ -21,6 +21,19 @@ class CategoryController extends AbstractController
 
     public function addAction(): void
     {
+        if ($_POST) {
+            $name = $_POST['name'];
+            $description = $_POST['description'];
+
+            $query = "INSERT INTO tb_category (name, description) VALUES ('{$name}', '{$description}')";
+
+            $con = Connection::getConnection();
+
+            $result = $con->prepare($query);
+            $result->execute();
+
+            echo 'Pronto, categoria inserida.';
+        }
         parent::render('category/add');
     }
 
