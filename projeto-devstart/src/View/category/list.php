90cd4146f859964ab8d030fe1124d199cbdeb5e4
nely de faria <nelly.nf54@gmail.com>
2022-06-13 17:17:26 -0300

    Criando botão editar

--- projeto-devstart/src/View/category/list.php
+++ projeto-devstart/src/View/category/list.php
@@ -21,6 +21,7 @@
                     echo "<td> {$name}</td>";
                     echo "<td> {$description}</td>";
                     echo "<td>
+                        <a href='/categorias/editar?id={$id}' class='btn btn-warning btn-sm'>Editar</a>
                         <a href='/categorias/excluir?id={$id}' class='btn btn-danger btn-sm'>Excluir</a>
                     </td>";
                 echo '</tr>';
