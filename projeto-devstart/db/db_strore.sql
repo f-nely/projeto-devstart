0428cac4a28f2c3a2332fa3841f5d92a11a8538d
nely de faria <nelly.nf54@gmail.com>
2022-06-13 18:28:59 -0300

    Inserindo dados na tabela tb_product

--- projeto-devstart/db/db_strore.sql
+++ projeto-devstart/db/db_strore.sql
@@ -19,13 +19,28 @@ CREATE TABLE tb_product
     name VARCHAR(30) NOT NULL,
     description VARCHAR(100) NOT NULL,
     photo VARCHAR(255) NOT NULL,
-    valor FLOAT(5, 2) NOT NULL,
-    categoria_id INT NOT NULL,
+    value FLOAT(5, 2) NOT NULL,
+    category_id INT NOT NULL,
     quantity INT NOT NULL,
     created_at DATETIME NOT NULL
 );
 
--- inserindo dados --
+-- excluindo tabela tb_product --
+DROP TABLE tb_product;
+
+-- inserindo dados na tabela tb_product --
+INSERT INTO tb_product
+(name, description, photo, value, category_id, quantity, created_at)
+VALUES
+('Teclado', 'Teclado mecânico gamer', 
+'https://media.pichau.com.br/media/catalog/product/cache/2f958555330323e505eba7ce930bdf27/m/c/mcr-ghl-rgb01445501.jpg', 
+199.89, 1, 50, '2022-05-10 09:30:34'),
+('Nobreak', 'Nobreak Linus 1200va 6 tomadas bivolt', 
+'https://img.kalunga.com.br/fotosdeprodutos/446914d.jpg', 799.00, 1, 10, '2022-06-09 11:49:51'),
+('Monitor', 'Monitor LED 21,5" Full HD 22MK400H LG CX 1 UN', 'https://img.kalunga.com.br/fotosdeprodutos/477270d.jpg',
+ 999.00, 1, 40, '2022-06-11 12:18:57');
+
+-- inserindo dados na tabela tb_category --
 INSERT INTO tb_category
 (name, description)
 VALUES
@@ -40,6 +55,9 @@ VALUES
 
 -- lista todas as categorias --
 SELECT * FROM tb_category;
+
+-- lista todas os produtos --
+SELECT * FROM tb_product;
 
 -- descrição da tabela tb_product --
 DESC tb_product;
