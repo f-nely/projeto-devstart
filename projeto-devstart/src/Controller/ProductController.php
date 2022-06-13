870b8021b39a78f77059add0f60bdffdc15eee5f
nely de faria <nelly.nf54@gmail.com>
2022-06-13 18:29:48 -0300

    Fazendo a implementação do método listAction

--- projeto-devstart/src/Controller/ProductController.php
+++ projeto-devstart/src/Controller/ProductController.php
@@ -4,12 +4,19 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Connection\Connection;
+
 class ProductController extends AbstractController
 {
     public function listAction(): void
     {
-        // include dirname(__DIR__) . '/View/product/list.php';
-        parent::render('product/list');
+        $con = Connection::getConnection();
+
+        $result = $con->prepare('SELECT * FROM tb_product');
+
+        $result->execute();
+
+        parent::render('product/list', $result);
     }
 
     public function addAction(): void
