7818bfbf3a196d2e16996b391a65390ab3fed6d8
nely de faria <nelly.nf54@gmail.com>
2022-06-14 17:44:33 -0300

    Alterando estilo do botão

--- projeto-devstart/src/View/product/list.php
+++ projeto-devstart/src/View/product/list.php
@@ -31,7 +31,7 @@
                         <td>{$quantity}</td>
                         <td>{$created_at}</td>
                         <td>
-                            <a href='/produtos/editar?id={$id}' class='btn btn-warning btn-sm'>Editar</a>
+                            <a href='/produtos/editar?id={$id}' class='btn btn-outline-warning btn-sm'>Editar</a>
                             <a href='/produtos/excluir?id={$id}' class='btn btn-outline-danger btn-sm'>Excluir</a>
                         </td>
                     </tr>
