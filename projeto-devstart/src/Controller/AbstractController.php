f7dd4f8adb09e759fddd090396433613ac5ed5f9
nely de faria <nelly.nf54@gmail.com>
2022-06-14 17:27:36 -0300

    Cria métodos renderMessage e includeHtml

--- projeto-devstart/src/Controller/AbstractController.php
+++ projeto-devstart/src/Controller/AbstractController.php
@@ -6,10 +6,20 @@ namespace App\Controller;
 
 abstract class AbstractController
 {
-    public function render(string $viewName, $data = null): void
+    public function includeHtml()
     {
         include dirname(__DIR__) . '/View/_partials/header.php';
-        include dirname(__DIR__) . "/View/{$viewName}.php";
         include dirname(__DIR__) . '/View/_partials/footer.php';
+    }
+    public function render(string $viewName, $data = null): void
+    {
+        self::includeHtml();
+        include dirname(__DIR__) . "/View/{$viewName}.php";
+    }
+
+    public function renderMessage(string $message): void
+    {
+        self::includeHtml();
+        include dirname(__DIR__) . "/View/_partials/message.php";
     }
 }
